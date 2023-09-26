e2913534373249915ecab3385e0b91a7eb9fa395
lucas <lucasmarcolino@uni9.edu.br>
2023-09-25 21:27:20 -0300

    alterando menu

--- menu.php
+++ menu.php
@@ -1,39 +1,20 @@
-<?php
-include "bootstrap.html";
-?>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+<header>
+        <div class="content">
+            <nav>
+                    <a href="index.php"><p class="brand">Dina<strong>Tech</strong></p></a>
+                <ul>
+                    <li><a href="#catalog">Catalogo</a></li>
+                    <li><a href="#about">Sobre</a></li>
+                    <li><a href="#feature">Recursos</a></li>
+                    <button>Login</button>
+                </ul>
+            </nav>
+            <div class="header-block">
+                <div class="text">
+                    <h2>O smartphone perfeito para você</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, optio.</p>
+                </div>
+                <img src="imagens/iphone-header.png" alt="Phone">
+            </div>
+        </div>
+    </header>
